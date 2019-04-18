4f0f9afd7438089dc244abc0bcdd0b8ee0bffd87
Vinicius <vinysales@hotmail.com>
2019-04-18 09:42:12 -0300

    adicionado efeitos ao scroll

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2-80
+        }, 800, 'swing', function () {
+            $(document).on("scroll", onScroll);
+        });
+    });
+  });
+  
+  function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#menu-center a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top-80 <= scrollPos && refElement.position().top-70 + refElement.height() > scrollPos) {
+            $('#menu-center ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+  }
+
+  /**********************************************************/
+
